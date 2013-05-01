d4bd09575d7f0c9851abb298355381d4a4a45535
VertxDev <bconnectlu@gmail.com>
2013-05-02 02:43:46 +0300

    Update user.js

--- app/model/user.js
+++ app/model/user.js
@@ -1,9 +1,11 @@
 module.exports = function(app, db){
-  
+	
 	var User = db.define('User', {
 	  username: Sequelize.STRING,
 	  password: Sequelize.STRING
-	})	
+	});
+	
+	User.sync();
 	
 	return User;
 }
